8cf871c1e623e090694f7f358ca650fa30932edd
Kealan McCusker <kealan.mccusker@certivox.com>
2016-09-20 16:06:19 +0100

    make app listen on 0.0.0.0

--- samples/flask_sample.py
+++ samples/flask_sample.py
@@ -102,4 +102,4 @@ if __name__ == "__main__":
     # Set debug flag for Flask app
     app.debug = True
     # and launch it on specified port
-    app.run(port=5000)
+    app.run(host='0.0.0.0',port=5000)
